54a471d0157f1c33dc8584b90d6f15a007a0b184
stetaf <s.tafuri95@gmail.com>
2021-06-15 10:52:04 +0200

    Add header dynamic links with vue

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,7 +7,32 @@
   \********************/
 /***/ (() => {
 
-
+/* App */
+var app = new Vue({
+  el: '#app',
+  data: {
+    links: [{
+      value: 'Home',
+      url: '#'
+    }, {
+      value: 'Courses',
+      url: '#'
+    }, {
+      value: 'Instructors',
+      url: '#'
+    }, {
+      value: 'Events',
+      url: '#'
+    }, {
+      value: 'Pages',
+      url: '#'
+    }, {
+      value: 'Elements',
+      url: '#'
+    }]
+  },
+  methods: {}
+});
 
 /***/ }),
 
---
+++ src/app.js
@@ -0,0 +1,36 @@
+/* App */
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        links: [
+            {
+                value: 'Home',
+                url: '#'
+            },
+            {
+                value: 'Courses',
+                url: '#'
+            },
+            {
+                value: 'Instructors',
+                url: '#'
+            },
+            {
+                value: 'Events',
+                url: '#'
+            },
+            {
+                value: 'Pages',
+                url: '#'
+            },
+            {
+                value: 'Elements',
+                url: '#'
+            }
+        ],
+    },
+    methods: {
+
+    }
+});
